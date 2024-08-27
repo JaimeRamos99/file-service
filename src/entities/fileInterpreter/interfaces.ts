e7199b875eb447133d25a3aec0af711ac303f5b4
Jaime Ramos <jrossetes@gmail.com>
2024-08-26 19:28:43 -0500

    feat: return extracted files from interpreter

---
+++ src/entities/fileInterpreter/interfaces.ts
@@ -0,0 +1,8 @@
+export enum ExtractedAttributes {
+  COMPANY_NAME = 'company_name',
+  EVENT_DATE = 'event_date',
+  EVENT_NAME = 'event_name',
+  PRICE = 'price',
+  PURCHASE_DATE = 'purchase_date',
+  time = 'time',
+}
