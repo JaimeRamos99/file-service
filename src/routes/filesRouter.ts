af535952766ab0d51211968bc4a22dab2745a98d
Jaime Ramos <jrossetes@gmail.com>
2024-10-29 18:51:07 -0500

    feature: inject dependencies using DI instead of composition

--- src/routes/filesRouter.ts
+++ src/routes/filesRouter.ts
@@ -2,9 +2,18 @@ import express from 'express';
 import { upload, checkFilePresence, rateLimiter } from '../middlewares';
 import { FileController } from '../controllers';
 import { RedisCacheAdapter } from '../cache/adapters/redis';
+import { FileService } from '../services';
+import { FileStorageManager, GCSStorageProvider } from '../integrations/fileStorage';
+import { FileInterpreterManager, GCPDocumentAI } from '../integrations/fileInterpreter';
 
 const filesRouter = express.Router();
-const fileController = new FileController(new RedisCacheAdapter());
+
+const fileController = new FileController(
+  new FileService(),
+  new FileStorageManager(new GCSStorageProvider()),
+  new FileInterpreterManager(new GCPDocumentAI()),
+  new RedisCacheAdapter(),
+);
 
 filesRouter.use(rateLimiter);
 filesRouter.get('/:fileUniqueName/signed-url', fileController.getSignedURL);
